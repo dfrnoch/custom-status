6f1a41222521835523bb86068344d02b15059c20
LnX <in@lnx.rip>
2020-08-29 00:06:57 +0200

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const Discord = require("discord.js");
-const axios = require('axios').default;
 var setTitle = require('console-title');
-const readline = require("readline")
-var center = require('center-align');
-var colors = require("colors")
-const notifier = require('node-notifier');
+const readline = require("readline");
+var color = require("chalk");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -14,18 +11,12 @@ const client = new Discord.Client();
 const {token} = require("./config.json");
 const {link} = require("./config.json");
 if(!token){
-    console.log(" ");
-    console.log(" ");
-    console.log(" ");
-    console.log(center("Token is not set! Edit it in config.json!".brightRed));
+    console.log(color.white('\n\n[') + color.red(' E ') + color.white(']') + color.gray("Token is not set! Edit it in config.json!"));
     setTimeout((function() {
         return process.exit(22);
     }), 100);}
 if(!link){
-        console.log(" ");
-        console.log(" ");
-        console.log(" ");
-        console.log(center("Link is not set! Edit it in config.json!".brightRed));
+        console.log(color.white('\n\n[') + color.red(' E ') + color.white(']') + color.gray("Link is not set! Edit it in config.json!"));
         setTimeout((function() {
             return process.exit(22);
         }), 100);}
@@ -33,42 +24,29 @@ if(!link.includes("https://twitch.tv/")){
         console.log(" ");
         console.log(" ");
         console.log(" ");
-        console.log(center("Link does not include https://twitch.tv/! Edit it in config.json!".brightRed));
+        console.log(color.white('[') + color.red(' E ') + color.white(']') + color.gray("Link does not include https://twitch.tv/! Edit it in config.json!"));
         setTimeout((function() {
             return process.exit(22);
         }), 100);}
+
+
 client.on('ready', async() => {
 	setTitle("Custom Status | Setup");
-	console.log(" ");
-	console.log(" ");
-	console.log(center(`               ╔═╗ ╔╦╗ ╔═╗ ╔╦╗ ╦ ╦ ╔═╗`.brightGreen, 112));
-	console.log(center(`               ╚═╗  ║  ╠═║  ║  ║ ║ ╚═╗`.gray, 112));
-    console.log(center(`               ╚═╝  ╩  ╩ ╩  ╩  ╚═╝ ╚═╝`.white, 112));
-    console.log(center(`
-    ╦══════════════════════════════╦
-    ║                              ║
-    ║   Custom Status displayer.   ║
-    ║       Made by lnx#0069       ║
-    ║                              ║
-    ╩══════════════════════════════╩
-    `.brightGreen, 112));
-
-console.log(" ");
-console.log(" ");
-let status = await question(('- Select what status you want to display (streaming, watching, playing, listening):'.brightGreen));
-let message = await question(('- Select message what you want to display:'.brightGreen));
+    console.log(color.white('[') + color.cyan(' § ') + color.white(']') + color.gray(' Discord Nitro Generator made by') + color.white(' LnX') + color.gray(' | Licensed under') + color.white(' MIT') + color.gray(' License'));
+    console.log(color.white('[') + color.cyan(' § ') + color.white(']') + color.gray(' You can follow me on Github:') + color.white(' https://github.com/lnxcz'));
+let status = await question(color.white('\n[') + color.yellow(' ? ') + color.white(']') + color.gray(' Select what status you want to display (streaming, watching, playing, listening): '));
+let message = await question(color.white('[') + color.yellow(' ? ') + color.white(']') + color.gray(' Select message what you want to display: '));
 var stat = await status.toLowerCase()
 
 if(message.length >= 100){
-    console.log(" ");
-    console.log("Error: Message is longer than 100 characters! Try again!".brightRed);
+    console.log(color.white('\n[') + color.red(' E ') + color.white(']') + color.gray(" Message is longer than 100 characters! Try again!"));
     setTimeout((function() {
         return process.exit(22);
     }), 100);
+
 }else if(stat === "streaming"){
     setTitle(`Displaying status for ${client.user.tag} | Streaming ${message}`);
-    console.log(" ")
-    console.log(`Displaying status for ${client.user.tag} | Streaming ${message}`.bgGreen);
+    console.log(color.white('\n[') + color.yellow(' ? ') + color.white(']') + color.gray(` Displaying status for `) + color.white(client.user.tag) + color.gray(" | Streaming ") + color.white(message));
     client.user.setPresence({
         game: {
             name: message,
@@ -78,7 +56,7 @@ if(message.length >= 100){
     })}else if(stat === "listening"){
         setTitle(`Displaying status for ${client.user.tag} | Listening ${message}`);
         console.log(" ")
-        console.log(`Displaying status for ${client.user.tag} | Listening ${message}`.bgGreen);
+        console.log(color.white('\n[') + color.yellow(' ? ') + color.white(']') + color.gray(` Displaying status for `) + color.white(client.user.tag) + color.gray(" | Listening ") + color.white(message));
         client.user.setPresence({
             game: {
                 name: message,
@@ -87,7 +65,7 @@ if(message.length >= 100){
         });}else if(stat === "playing"){
             setTitle(`Displaying status for ${client.user.tag} | Playing ${message}`);
             console.log(" ")
-            console.log(`Displaying status for ${client.user.tag} | Playing ${message}`.bgGreen);
+            console.log(color.white('\n[') + color.yellow(' ? ') + color.white(']') + color.gray(` Displaying status for `) + color.white(client.user.tag) + color.gray(" | Playing ") + color.white(message));
             client.user.setPresence({
                 game: {
                     name: message,
@@ -95,16 +73,14 @@ if(message.length >= 100){
                 }
             });}else if(stat === "watching"){
                 setTitle(`Displaying status for ${client.user.tag} | Watching ${message}`);
-                console.log(" ")
-                console.log(`Displaying status for ${client.user.tag} | Watching ${message}`.bgGreen);
+                console.log(color.white('\n[') + color.yellow(' ? ') + color.white(']') + color.gray(` Displaying status for `) + color.white(client.user.tag) + color.gray(" | Watching ") + color.white(message));
                 client.user.setPresence({
                     game: {
                         name: message,
                         type: "WATCHING",
                     }
                 });}else{
-                    console.log(" ");
-                    console.log("Error: Status is misspelled! Try again!".brightRed);
+                    console.log(color.white('\n[') + color.red(' E ') + color.white(']') + color.gray(" Status is misspelled! Try again!"));
                     process.exit
                     setTimeout((function() {
                         return process.exit(22);
